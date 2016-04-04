eef7b7fae1b3ff636bbf187e04799800697eeffc
Anthony Larios <simpsonsruleA52@gmail.com>
2016-04-04 12:39:47 -0700

    Header now changes to the clicked color when it is correct

--- color_game.js
+++ color_game.js
@@ -4,6 +4,7 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,6 +20,7 @@ for (var i = 0; i < squares.length; i++) {
     if(clickedColor === pickedColor) {
       messageDisplay.textContent = "Correct";
       changeColors(pickedColor);
+      h1.style.background = pickedColor;
     } else {
       this.style.background = "#232323";
       messageDisplay.textContent = "Try again"
